c80b588c4b68b0f56880fcba73d3b3b3753c8343
farahbillimoria <78675690+farahbillimoria@users.noreply.github.com>
2021-03-21 11:40:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,31 @@
+var movingrectangle;
+var fixedrectangle;
+
+
+
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,1200);
+ movingrectangle=createSprite(600,600,100,50);
+ movingrectangle.shapeColor="purple";
+ fixedrectangle=createSprite(800,800,50,100);
+ fixedrectangle.shapeColor="purple";
 }
 
 function draw() {
-  background(255,255,255);  
+  background("yellow");
+  movingrectangle.x=World.mouseX;
+  movingrectangle.y=World.mouseY;  
+  if(movingrectangle.x-fixedrectangle.x<fixedrectangle.width/2+movingrectangle.width/2&&
+    fixedrectangle.x-movingrectangle.x<fixedrectangle.width/2+movingrectangle.width/2&&
+    movingrectangle.y-fixedrectangle.y<fixedrectangle.height/2+movingrectangle.height/2&&
+    fixedrectangle.y-movingrectangle.y<fixedrectangle.height/2+movingrectangle.height/2 ){
+    movingrectangle.shapeColor="blue";
+    fixedrectangle.shapeColor="blue";
+  }else{
+    movingrectangle.shapeColor="purple";
+    fixedrectangle.shapeColor="purple";
+  }
   drawSprites();
 }
